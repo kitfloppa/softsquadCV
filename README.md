5a2c67436072b6d27c7e477b66bc845d70eb47ce
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-06-18 20:23:38 +1000

    rewrite calc_metric

--- README.md
+++ README.md
@@ -13,4 +13,8 @@ pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable
 
 ## Task № 2
 
-Данные хранятся в папке «input_dir» (description.csv, data, image_counter.txt), туда же сохранятся файл «output_cars.csv» с результатами.
+Данные хранятся в папке «input_dir_cars» (description.csv, data, image_counter.txt), туда же сохранятся файл «output_cars.csv» с результатами.
+
+## Task № 3
+
+Реализована функция «calc_metric» для нахождения доминирующего цвета в выделенной области.
--- main.py
+++ main.py
@@ -8,21 +8,25 @@ import numpy as np
 def create_bar(height, width, color):
     bar = np.zeros((height, width, 3), np.uint8)
     bar[:] = color
-    return bar
+    red, green, blue = int(color[2]), int(color[1]), int(color[0])
+    return bar, (red, green, blue)
 
 
 def calc_metric(image, x, y, w, h):
+    image = image[(y - h):y, x:(x + w)]
     cv2.imshow('image', image)
-    image = image[y:y + h, x:x + w]
     data = np.float32(image).reshape((-1, 3))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    ret, label, center = cv2.kmeans(data, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    ret, label, center = cv2.kmeans(data, 5, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    percentages = (np.unique(label, return_counts=True)[1]) / data.shape[0]
 
-    bar = create_bar(400, 200, center[0])
+    bar, color = create_bar(400, 200, center[np.argmax(percentages)])
     img_bar = np.hstack((bar,))
 
     cv2.imshow('Dominant colors', img_bar)
     cv2.waitKey(0)
+
+    return img_bar, color
 
 
 def find_car(input_dir, output_cars ='output.csv'):
@@ -40,8 +44,6 @@ def find_car(input_dir, output_cars ='output.csv'):
 
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
     results = model(imgs)
-
-    #print(results.pandas().xyxy[70])
 
     if not os.path.exists(output_cars):
         with open(output_cars, 'w', newline='') as f:
@@ -68,7 +70,6 @@ def merge_channels(input_dir, output_dir):
                 cv2.imwrite(output_dir + f'/{file_name[:5]}.jpg', merge_image)
     
 if __name__ == '__main__':
-    #find_car('input_dir', 'output_cars.csv')
 
-    img = cv2.imread('input_dir/rgb_data/00071.jpg')
-    calc_metric(img, 106, 621, 300, 800)
+    img = cv2.imread('input_dir_cars/rgb_data/00071.jpg')
+    calc_metric(img, 106, 621, 900, 300)

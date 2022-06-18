db057afb13f77b4b8649fa9fa93f3466e002903e
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-06-18 17:30:26 +1000

    add calc metric (task 3)

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ import torch
 import numpy as np
 
 
+def create_bar(height, width, color):
+    bar = np.zeros((height, width, 3), np.uint8)
+    bar[:] = color
+    return bar
+
+
+def calc_metric(image, x, y, w, h):
+    cv2.imshow('image', image)
+    image = image[y:y + h, x:x + w]
+    data = np.float32(image).reshape((-1, 3))
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, label, center = cv2.kmeans(data, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    bar = create_bar(400, 200, center[0])
+    img_bar = np.hstack((bar,))
+
+    cv2.imshow('Dominant colors', img_bar)
+    cv2.waitKey(0)
+
+
 def find_car(input_dir, output_cars ='output.csv'):
     os.chdir(input_dir)
     input_dir, output_dir = 'data', 'rgb_data'
@@ -20,6 +40,8 @@ def find_car(input_dir, output_cars ='output.csv'):
 
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
     results = model(imgs)
+
+    #print(results.pandas().xyxy[70])
 
     if not os.path.exists(output_cars):
         with open(output_cars, 'w', newline='') as f:
@@ -46,5 +68,7 @@ def merge_channels(input_dir, output_dir):
                 cv2.imwrite(output_dir + f'/{file_name[:5]}.jpg', merge_image)
     
 if __name__ == '__main__':
-    find_car('input_dir', 'output_cars.csv')
-    
+    #find_car('input_dir', 'output_cars.csv')
+
+    img = cv2.imread('input_dir/rgb_data/00071.jpg')
+    calc_metric(img, 106, 621, 300, 800)

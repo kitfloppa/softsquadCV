bb6e32c0b3bbabdead77a23e3327469acb4eabb7
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-06-18 16:28:34 +1000

    add detection (Task 2)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# softsquadCV
-vezdecode tasks
+## Installation Python packages
+
+```bash
+python -m venv .venv
+.\.venv\Scripts\activate
+
+pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+```
+
+## Task № 1
+
+Все предобработанные данные сохраняются в папку «rgb_data»
+
+## Task № 2
+
+Данные хранятся в папке «input_dir» (description.csv, data, image_counter.txt), туда же сохранятся файл «output_cars.csv» с результатами.
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import os
 import cv2
+import csv
+import torch
 import numpy as np
+
+
+def find_car(input_dir, output_cars ='output.csv'):
+    os.chdir(input_dir)
+    input_dir, output_dir = 'data', 'rgb_data'
+    cars = ['car', 'truck', 'bus']
+    imgs = []
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+        merge_channels(input_dir, output_dir)
+
+    for file_name in os.listdir(output_dir):
+        imgs.append(cv2.imread(os.path.join(output_dir, file_name)))
+
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    results = model(imgs)
+
+    if not os.path.exists(output_cars):
+        with open(output_cars, 'w', newline='') as f:
+            writer = csv.writer(f)
+
+            for i, file_name in enumerate(os.listdir(output_dir)):
+                res = [n in results.pandas().xyxy[i]['name'].unique() for n in cars]
+
+                writer.writerow([file_name, bool(sum(res))])
+
 
 def merge_channels(input_dir, output_dir):
     data = os.listdir(input_dir)
@@ -14,14 +43,8 @@ def merge_channels(input_dir, output_dir):
                 count = 0
                 merge_image = cv2.merge([channels[0], channels[1], channels[2]])
                 channels = []
-                cv2.imwrite(output_dir + f'/{file_name[:6]}rgb.jpg', merge_image)
+                cv2.imwrite(output_dir + f'/{file_name[:5]}.jpg', merge_image)
     
 if __name__ == '__main__':
-    image_count = open('image_counter.txt').read()
-    input_dir, output_dir = 'data', 'rgb_data'
-    
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    merge_channels(input_dir, output_dir)
+    find_car('input_dir', 'output_cars.csv')
     
